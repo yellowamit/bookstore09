9b975074a08fc5d2192fb6abba0ede0c2c5d31a1
yellowamit <mail2amikg@gmail.com>
2024-07-17 19:47:18 +0530

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:9811@cluster99.qr1esn5.mongodb.net/books?retryWrites=true&w=majority&appName=Cluster99';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
